848f349b6e42451ba380b685b97d24dc728cd68a
Ivan Vitjuk <virtus@eclipsedminds.org>
2018-07-04 22:39:38 -0700

    Fix for the previous commit

--- scripts/docker_clean.sh
+++ scripts/docker_clean.sh
@@ -2,8 +2,8 @@
 
 echo "Removing unused containers"
 containers=$(docker ps -qa --no-trunc --filter "status=exited")
-[ ! -z "$containers" ] && docker rm "$containers"
+[ ! -z "$containers" ] && docker rm $containers
 
 echo "Removing unused images"
 images=$(docker images --filter "dangling=true" -q --no-trunc)
-[ ! -z "$images" ] && docker rmi "$images"
+[ ! -z "$images" ] && docker rmi $images
